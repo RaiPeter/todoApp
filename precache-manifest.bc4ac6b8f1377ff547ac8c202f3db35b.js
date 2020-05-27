bf145cc9ff75fe906cf82444fdbe132022c674b0
RaiPeter <rai.peter2@gmail.com>
2020-05-28 00:30:00 +0530

    Updates

---
+++ precache-manifest.bc4ac6b8f1377ff547ac8c202f3db35b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b81a42ae57dcb7e906d0a1c2484ca303",
+    "url": "/todoApp/index.html"
+  },
+  {
+    "revision": "55461d761bcb22385dc1",
+    "url": "/todoApp/static/css/main.104b3186.chunk.css"
+  },
+  {
+    "revision": "1f3ca6c2007277fd6d78",
+    "url": "/todoApp/static/js/2.99a0c5ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoApp/static/js/2.99a0c5ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55461d761bcb22385dc1",
+    "url": "/todoApp/static/js/main.b4cf3fb8.chunk.js"
+  },
+  {
+    "revision": "c739c260f1a9ca7786c0",
+    "url": "/todoApp/static/js/runtime-main.425b0c3b.js"
+  }
+]);
